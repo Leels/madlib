06e984aef5f67f61782de98a28dc47296239d5a2
Student <example@example.com>
2019-10-28 09:46:43 -0700

    add loops to simplify code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,12 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var dateInput = $("input#date").val();
-    var nameInput = $("input#name").val();
-    var exclamationInput= $("input#exclamation").val();
-    var locationInput = $("input#location").val();
-    var adjetiveInput = $("input#adjetive").val();
-    var celebrity1Input = $("input#celebrity1").val();
-    var celebrity2Input = $("input#celebrity2").val();
-    var amountInput = $("input#amount").val();
-    var numberInput = $("input#number").val();
-    var housingTypeInput = $("input#housingType").val();
-    var animalInput = $("input#animal").val();
+    var inputs = ["date", "name", "exclamation", "location", "adjetive", "celebrity1", "celebrity2", "amount", "number", "housingType", "animal"]
 
-    $(".date").text(dateInput);
-    $(".name").text(nameInput);
-    $(".exclamation").text(exclamationInput);
-    $(".location").text(locationInput);
-    $(".adjetive").text(adjetiveInput);
-    $(".celebrity1").text(celebrity1Input);
-    $(".celebrity2").text(celebrity2Input);
-    $(".amount").text(amountInput);
-    $(".number").text(numberInput);
-    $(".housingType").text(housingTypeInput);
-    $(".animal").text(animalInput);
+  inputs.forEach(function(input) {
+    var all = $("input#" + inputs).val();
+    $("." + input).text(all);
+  });
+
 
     $("#letter").show();
 
