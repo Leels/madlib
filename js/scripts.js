9a5398d6b8c9d2b97718a7e9f70c5ad07142260a
Lela Smith <smith.lelaa@gmail.com>
2020-12-17 17:43:31 -0800

    add better variable names

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var inputs = ["date", "name", "exclamation", "location", "adjetive", "celebrity1", "celebrity2", "amount", "number", "housingType", "animal"]
 
-  inputs.forEach(function(input) {
-    var all = $("input#" + input).val();
-    $("." + input).text(all);
-  });
+    let words = ["date", "name", "exclamation", "location", "adjetive", "celebrity1", "celebrity2", "amount", "number", "housingType", "animal"]
+
+    words.forEach(function(word) {
+      let input = $("input#" + word).val();
+      $("." + word).text(input);
+    });
 
     $("#letter").show();
 
